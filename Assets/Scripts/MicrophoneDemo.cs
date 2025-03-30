0dd402bf10c551d84e6606dab189c120028b57d8
Lee Ze Hao <zehaolee2004@gmail.com>
2025-03-31 01:27:02 +0800

    Add speech to text system

---
+++ Assets/Scripts/MicrophoneDemo.cs
@@ -0,0 +1,88 @@
+using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Whisper.Utils;
+using Button = UnityEngine.UI.Button;
+
+namespace Whisper.Samples
+{
+    /// <summary>
+    /// Record audio clip from microphone and make a transcription.
+    /// </summary>
+    public class MicrophoneDemo : MonoBehaviour
+    {
+        public WhisperManager whisper;
+        public MicrophoneRecord microphoneRecord;
+        public bool streamSegments = true;
+        public bool printLanguage = true;
+
+        [Header("UI")] 
+        public Button button;
+        public TMP_Text buttonText;
+        public TMP_InputField outputText;
+        
+        private string _buffer;
+
+        private void Awake()
+        {
+            whisper.OnNewSegment += OnNewSegment;
+            whisper.OnProgress += OnProgressHandler;
+            
+            microphoneRecord.OnRecordStop += OnRecordStop;
+            
+            button.onClick.AddListener(OnButtonPressed);
+            whisper.language = "en";
+        }
+
+        private void OnButtonPressed()
+        {
+            if (!microphoneRecord.IsRecording)
+            {
+                microphoneRecord.StartRecord();
+                buttonText.text = "Stop";
+            }
+            else
+            {
+                microphoneRecord.StopRecord();
+                buttonText.text = "STT";
+            }
+        }
+        
+        private async void OnRecordStop(AudioChunk recordedAudio)
+        {
+            buttonText.text = "Record";
+            _buffer = "";
+
+            var sw = new Stopwatch();
+            sw.Start();
+            
+            var res = await whisper.GetTextAsync(recordedAudio.Data, recordedAudio.Frequency, recordedAudio.Channels);
+            if (res == null || !outputText) 
+                return;
+
+            var time = sw.ElapsedMilliseconds;
+            var rate = recordedAudio.Length / (time * 0.001f);
+
+            var text = res.Result;
+            if (printLanguage)
+                text += $"\n\nLanguage: {res.Language}";
+            
+            outputText.text = outputText.text + " " + text;
+        }
+
+        private void OnProgressHandler(int progress)
+        {
+
+        }
+        
+        private void OnNewSegment(WhisperSegment segment)
+        {
+            if (!streamSegments || !outputText)
+                return;
+
+            _buffer += segment.Text;
+            outputText.text = _buffer + "...";
+        }
+    }
+}
